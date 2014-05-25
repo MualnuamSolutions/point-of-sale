17cae35d98bbe4137555db619a7cd5f2f8cd6016
Alan Pachuau <alanpachuau@gmail.com>
2014-05-25 23:18:34 +0530

    add stocks menu on top and sidebar menu

--- app/views/partials/menu.blade.php
+++ app/views/partials/menu.blade.php
@@ -1,6 +1,7 @@
 @if(isset($position) && $position == 'top')
 <div class="hidden-xs" id="topbar_menu">
    <ul class="nav navbar-nav navbar-right">
+      <li><a class="tooltip-bottom" title="Stocks" data-toggle="tooltip" href="{{ route('stocks.index') }}"><i class="fi-database"></i></a></li>
       <li><a class="tooltip-bottom" title="Products" data-toggle="tooltip" href="{{ route('products.index') }}"><i class="fa fa-gift"></i></a></li>
       <li><a class="tooltip-bottom" title="Suppliers" data-toggle="tooltip" href="{{ route('suppliers.index') }}"><i class="fa fa-truck"></i></a></li>
       <li><a class="tooltip-bottom" title="Customers" data-toggle="tooltip" href="{{ route('customers.index') }}"><i class="fa fa-users"></i></a></li>
@@ -14,6 +15,7 @@
 @if(isset($position) && $position == 'sidebar')
 <div class="visible-xs col-xs-6 sidebar-offcanvas" id="sidebar">
    <div class="list-group">
+      <a href="{{ route('stocks.index') }}" class="list-group-item"><i class="fi-database"></i> Stocks</a>
       <a href="{{ route('products.index') }}" class="list-group-item"><i class="fa fa-gift"></i> Products</a>
       <a href="{{ route('suppliers.index') }}" class="list-group-item"><i class="fa fa-truck"></i> Suppliers</a>
       <a href="{{ route('customers.index') }}" class="list-group-item"><i class="fa fa-users"></i> Units</a>
