15732010048aff2a58033c181fc331bb9a86d829
Mualnuamm Solutions <mualnuamsolutions@gmail.com>
2014-06-15 01:09:33 +0530

    new migrate of outlet_deposits
    POS

---
+++ app/database/migrations/2014_06_15_005818_create_outlet_deposits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOutletDepositsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('outlet_deposits',function($table)
+      {
+         $table->increments('id');
+         $table->integer('outlet_id');
+         $table->float('deposit_amt');
+         $table->float('refference_no');
+         $table->integer('status');// approval for manager (approved/not approved)
+         $table->timestamps();
+      });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('outlet_deposits');
+	}
+
+}
